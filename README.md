b9584ae257df5a7c9bae1bb92bba68011ed66c9b
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-01 14:21:50 -0700

    home view routed to app.jsx

---
+++ README.md
@@ -0,0 +1,29 @@
+# Noah's Dev Site
+
+## All of the ideas
+
+- About me page
+
+- Past Projects
+
+  - academic
+  - motorcycles
+  - metal art
+  - knives
+
+- Links / contacts
+
+  - github
+  - linkedIn
+  - email
+  - instagram?
+  - resume
+
+- Data
+  - pictures
+    - drive
+    - instagram
+    - google photos
+  -
+
+## views
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export default function App() {
-  return <h1>Hello World</h1>;
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './views/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+export default App;
