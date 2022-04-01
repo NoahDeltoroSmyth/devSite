3140f29e045a908757d7cb5c09fbe97dc3415819
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-01 14:34:10 -0700

    basic routes layed out on home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Bio</h1>
+      <div>
+        <h2>Contact</h2>
+        <h2>Personal Projects</h2>
+        <h2>Bio</h2>
+        <h2>Code Projects</h2>
+      </div>
+    </>
+  );
 };
 
 export default Home;
