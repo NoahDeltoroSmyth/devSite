945c88c947596ae832dc12750fd9628c4496fb35
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-01 16:01:40 -0700

    4 links to contact me from

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>Contact me</h1>
+      <a href="https://github.com/NoahDeltoroSmyth" target="_blank">
+        <p>Github</p>
+      </a>
+      <a href="https://www.linkedin.com/in/noahdeltorosmyth/" target="_blank">
+        <p>Linkedin</p>
+      </a>
+      <a href="mailto:noahsmyth96@gmail.com">
+        <p>Email: Noahsmyth96@gmail.com</p>
+      </a>
+      <a href="https://www.canva.com/design/DAE4pvlZQA0/view" target="_blank">
+        <p>Resume</p>
+      </a>
+    </div>
+  );
 };
 
 export default Contact;
