e6fddc1139d16c5a93eb249b6689b2c7d5c09d6b
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-01 14:56:49 -0700

    end routes are linked and accessible with button click

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const Home = () => {
   return (
     <>
       <h1>Bio</h1>
       <div>
-        <h2>Contact</h2>
-        <h2>Personal Projects</h2>
-        <h2>Bio</h2>
-        <h2>Code Projects</h2>
+        <Link to="/contact">
+          <h2>Contact</h2>
+        </Link>
+        <Link to="personal-projects">
+          <h2>Personal Projects</h2>
+        </Link>
+        <Link to="/">
+          <h2>Bio</h2>
+        </Link>
+        <Link to="code-projects">
+          <h2>Code Projects</h2>
+        </Link>
       </div>
     </>
   );
